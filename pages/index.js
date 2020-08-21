96275f9956320a2a7ea07535f4dced6d11cbb17b
tvergho <tvergho@gmail.com>
2020-08-21 16:20:24 -0700

    Starter

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+
+const Home = () => {
+  return (
+    <>
+      <NextSeo
+        title="Home"
+        description="Description."
+      />
+    </>
+  );
+};
+
+export default Home;
